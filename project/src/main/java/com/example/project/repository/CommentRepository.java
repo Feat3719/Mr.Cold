48be2f3b2da047cfdb4cd26daf7e58ce50beff27
limPage <119988213+limPage@users.noreply.github.com>
2023-10-14 17:07:40 +0900

    Delete project directory

--- project/src/main/java/com/example/project/repository/CommentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.project.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.project.entity.Comment;
-
-public interface CommentRepository extends JpaRepository<Comment, Integer>{
-    
-}
